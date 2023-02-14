f69060a8c33caa58c7dcd8bc2a23cda1e549fc2a
Vincent <vcannall@gmail.com>
2023-02-13 17:42:21 -0800

    refactoring unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,8 +12,9 @@ const StatisticLine = ({ text, value }) => (
 );
 
 const Statistics = ({ good, neutral, bad }) => {
-  let all = good + neutral + bad;
   if (good || neutral || bad) {
+    let all = good + neutral + bad;
+
     return (
       <table>
         <tbody>
@@ -23,9 +24,12 @@ const Statistics = ({ good, neutral, bad }) => {
           <StatisticLine text="all" value={all}></StatisticLine>
           <StatisticLine
             text="average"
-            value={(good * 1 + neutral * 0 + bad * -1) / all}
+            value={(good - bad) / all}
           ></StatisticLine>
-          <StatisticLine text="positive" value={good / all}></StatisticLine>
+          <StatisticLine
+            text="positive"
+            value={(100 * good) / all + "%"}
+          ></StatisticLine>
         </tbody>
       </table>
     );
@@ -40,19 +44,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const setToValue = (value, setter) => {
-    setter(value);
-  };
-
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setToValue(good + 1, setGood)} text="good" />
-      <Button
-        handleClick={() => setToValue(neutral + 1, setNeutral)}
-        text="neutral"
-      />
-      <Button handleClick={() => setToValue(bad + 1, setBad)} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
