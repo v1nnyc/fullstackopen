1befd95f76447d2d9c02cdb1a3a71b5b4a3dd729
Vincent <vcannall@gmail.com>
2023-02-25 16:05:09 -0800

    fixed other error in practice w notes

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -67,8 +67,6 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
-
-  response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
